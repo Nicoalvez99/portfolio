8c9f6019d7b99bc1cd35f169f65560a67994cb00
Nicolas Alvez <nicoalvez99@gmail.com>
2022-07-15 15:21:33 -0300

    Cambios

--- contacto.php
+++ contacto.php
@@ -14,29 +14,7 @@
 </head>
 <body id="contacto">
     <header class="container">
-        <nav class="navbar navbar-expand-md mb-4">
-            <div class="container-fluid">
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                  <li class="nav-item">
-                    <a class="nav-link " href="index.html">Inicio</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="sobre-mi.html">Sobre mi</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="proyectos.html">Proyectos</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link active" href="contacto.html">Contacto</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
+       <?php include_once("menu.php"); ?>
     </header>
     <main class="container">
         <div class="row">
@@ -82,7 +60,7 @@
           <a href="mailto:nicoalvez@gmail.com">nicoalvez99@gmail.com</a>
         </div>
         <div class="col-3">
-          <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+          <a href="https://api.whatsapp.com/send?phone=541157351589"><i class="fa-brands fa-whatsapp"></i></a>
         </div>
       </div>
 
---
+++ menu.php
@@ -0,0 +1,24 @@
+    <nav class="navbar navbar-expand-md mb-4">
+            <div class="container-fluid">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" href="index.html">Inicio</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="sobre-mi.html">Sobre mi</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="proyectos.html">Proyectos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link " href="contacto.html">Contacto</a>
+                  </li>
+                </ul>
+                <a class="cv" href="#">Descargá mi CV<i class="fa-solid fa-download"></i></a>                
+              </div>
+            </div>
+          </nav>
